f70fe4fc9b963a864f815496c8113c403e5d91c6
Juan David Camargo <juandavidmoracamargo58@gmail.com>
2022-03-29 20:17:53 -0500

    Finished pc.py update

--- Fc.py
+++ Fc.py
@@ -1,61 +1,25 @@
-def fc_rn(latidos):
+def fc_lactantes(latidos):
     fc = int(latidos * 4)
-    if fc < 120:
+    if fc < 100:
         print(f"""
+        La edad del paciente pertenece al grupo lactante.
+              
         La frecuencia cardiaca de tu paciente es {str(fc)}.
         
         Tu paciente se encuentra bradicardico.""")
 
     elif fc > 140:
         print(f"""
+        La edad del paciente pertenece al grupo lactante.
+              
         La frecuencia cardiaca de tu paciente es {str(fc)}.
         
         Tu paciente se encuentra taquicardico.""")
 
     else:
         print(f"""
-        La frecuencia cardiaca de tu paciente es {str(fc)}.
-        
-        Tu paciente se encuentra en los rangos normales de frecuencia cardiaca.""")
-
-
-def fc_lactante_menor(latidos):
-    fc = int(latidos * 4)
-    if fc < 100:
-        print(f"""
-        La frecuencia cardiaca de tu paciente es {str(fc)}.
-        
-        Tu paciente se encuentra bradicardico.""")
-
-    elif fc > 130:
-        print(f"""
-        La frecuencia cardiaca de tu paciente es {str(fc)}.
-        
-        Tu paciente se encuentra taquicardico.""")
-
-    else:
-        print(f"""
-        La frecuencia cardiaca de tu paciente es {str(fc)}.
-        
-        Tu paciente se encuentra en los rangos normales de frecuencia cardiaca.""")
-
-
-def fc_lactante_mayor(latidos):
-    fc = int(latidos * 4)
-    if fc < 100:
-        print(f"""
-        La frecuencia cardiaca de tu paciente es {str(fc)}.
-        
-        Tu paciente se encuentra bradicardico.""")
-
-    elif fc > 120:
-        print(f"""
-        La frecuencia cardiaca de tu paciente es {str(fc)}.
-        
-        Tu paciente se encuentra taquicardico.""")
-
-    else:
-        print(f"""
+        La edad del paciente pertenece al grupo lactante.
+              
         La frecuencia cardiaca de tu paciente es {str(fc)}.
         
         Tu paciente se encuentra en los rangos normales de frecuencia cardiaca.""")
@@ -65,18 +29,24 @@ def fc_preescolar(latidos):
     fc = int(latidos * 4)
     if fc < 80:
         print(f"""
+        La edad del paciente pertenece al grupo pre-escolar.      
+        
         La frecuencia cardiaca de tu paciente es {str(fc)}.
         
         Tu paciente se encuentra bradicardico.""")
 
     elif fc > 120:
         print(f"""
+        La edad del paciente pertenece al grupo pre-escolar.
+              
         La frecuencia cardiaca de tu paciente es {str(fc)}.
         
         Tu paciente se encuentra taquicardico.""")
 
     else:
         print(f"""
+        La edad del paciente pertenece al grupo pre-escolar.
+              
         La frecuencia cardiaca de tu paciente es {str(fc)}.
         
         Tu paciente se encuentra en los rangos normales de frecuencia cardiaca.""")
@@ -86,18 +56,24 @@ def fc_escolar(latidos):
     fc = int(latidos * 4)
     if fc < 80:
         print(f"""
+        La edad del paciente pertenece al grupo escolar.
+              
         La frecuencia cardiaca de tu paciente es {str(fc)}.
         
         Tu paciente se encuentra bradicardico.""")
 
     elif fc > 100:
         print(f"""
+        La edad del paciente pertenece al grupo escolar.
+              
         La frecuencia cardiaca de tu paciente es {str(fc)}.
         
         Tu paciente se encuentra taquicardico.""")
 
     else:
         print(f"""
+        La edad del paciente pertenece al grupo escolar.
+              
         La frecuencia cardiaca de tu paciente es {str(fc)}.
         
         Tu paciente se encuentra en los rangos normales de frecuencia cardiaca.""")
@@ -107,18 +83,24 @@ def fc_adolecente(latidos):
     fc = int(latidos * 4)
     if fc < 70:
         print(f"""
+        La edad del paciente pertenece al grupo adolecente.
+              
         La frecuencia cardiaca de tu paciente es {str(fc)}.
         
         Tu paciente se encuentra bradicardico.""")
 
     elif fc > 100:
         print(f"""
+        La edad del paciente pertenece al grupo adolecente.
+              
         La frecuencia cardiaca de tu paciente es {str(fc)}.
         
         Tu paciente se encuentra taquicardico.""")
 
     else:
         print(f"""
+        La edad del paciente pertenece al grupo adolecente.
+              
         La frecuencia cardiaca de tu paciente es {str(fc)}.
         
         Tu paciente se encuentra en los rangos normales de frecuencia cardiaca.""")
@@ -128,18 +110,24 @@ def fc_adulto(latidos):
     fc = int(latidos * 4)
     if fc < 60:
         print(f"""
+        La edad del paciente pertenece al grupo adulto.
+              
         La frecuencia cardiaca de tu paciente es {str(fc)}.
         
         Tu paciente se encuentra bradicardico.""")
 
     elif fc > 100:
         print(f"""
+        La edad del paciente pertenece al grupo adulto.
+              
         La frecuencia cardiaca de tu paciente es {str(fc)}.
         
         Tu paciente se encuentra taquicardico.""")
 
     else:
         print(f"""
+        La edad del paciente pertenece al grupo adulto.
+              
         La frecuencia cardiaca de tu paciente es {str(fc)}.
         
         Tu paciente se encuentra en los rangos normales de frecuencia cardiaca.""")
@@ -149,32 +137,24 @@ def run():
     edad_paciente = int(input("Años de tu paciente: "))
     latidos_paciente = int(input("Cuantos latidos encontraste en 15 segundos: "))
 
-    if edad_paciente == 1:
-        latidos = int(input("Cuantos latidos encotraste en tu paciente en 15 segundos?: "))
-        fc_rn(latidos)
+    if edad_paciente <= 1:
+        latidos = latidos_paciente
+        fc_lactantes(latidos)
 
-    elif edad_paciente == 2:
-        latidos = int(input("Cuantos latidos encotraste en tu paciente en 15 segundos?: "))
-        fc_lactante_menor(latidos)
-
-    elif edad_paciente == 3:
-        latidos = int(input("Cuantos latidos encotraste en tu paciente en 15 segundos?: "))
-        fc_lactante_mayor(latidos)
-
-    elif menu == 4:
-        latidos = int(input("Cuantos latidos encotraste en tu paciente en 15 segundos?: "))
+    elif edad_paciente <= 6:
+        latidos = latidos_paciente
         fc_preescolar(latidos)
 
-    elif menu == 5:
-        latidos = int(input("Cuantos latidos encotraste en tu paciente en 15 segundos?: "))
+    elif edad_paciente <= 13:
+        latidos = latidos_paciente
         fc_escolar(latidos)
 
-    elif menu == 6:
-        latidos = int(input("Cuantos latidos encotraste en tu paciente en 15 segundos?: "))
+    elif edad_paciente <= 17:
+        latidos = latidos_paciente
         fc_adolecente(latidos)
 
-    elif menu == 7:
-        latidos = int(input("Cuantos latidos encotraste en tu paciente en 15 segundos?: "))
+    elif edad_paciente >= 18:
+        latidos = latidos_paciente
         fc_adulto(latidos)
 
     else:
